bd743eace13a671d3d9445373a406e4325d96a1c
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-02 15:27:23 +0600

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,15 @@
+/**
+ * ----------------------------
+ * ONE TIME For Your Computer
+ * ----------------------------
+ * 1.create heroku account
+ * 2.verify email
+ * 3.install heroku cli
+ * 4.heroku login
+ * -----------------------------
+ * For Each Project One Time
+ * 1.heroku create
+ * 
+ * 
+ * 
+ */ 
