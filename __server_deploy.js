e4f74a24f2a4d3a6cdf271087a0323020c20dbc5
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-03 17:17:00 +0600

    adding steps

--- __server_deploy.js
+++ __server_deploy.js
@@ -8,7 +8,11 @@
  * 4.heroku login
  * -----------------------------
  * For Each Project One Time
+ * ----------------------------
  * 1.heroku create
+ * 2.make sure you:git add . git commit -m"" git push
+ * 3.git push heroku main
+ * 4.
  * 
  * 
  * 
